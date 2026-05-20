752920c80453d9e6907e36f9ca137af0e8b55f4a
George Carpinisan <gcarpinisan.04@gmail.com>
2026-05-20 17:45:51 +0300

    Change mod.rs and retire old reset_phase() function in classic_osc (formerly used for the kick drum)
    Signed-off-by: George Carpinisan <gcarpinisan.04@gmail.com>

--- src/synth/mod.rs
+++ src/synth/mod.rs
@@ -2,9 +2,11 @@ pub mod adsr;
 pub mod classic_osc;
 pub mod pulse_osc;
 pub mod svf_filter;
+pub mod effects;
 
 pub use classic_osc::{WtableUnison, WtableOscillator, make_wtable};
 pub use classic_osc::{wave_sine, wave_saw, wave_triangle};
 pub use pulse_osc::{PulseUnison};
 pub use svf_filter::SvfFilter;
 pub use adsr::AdsrEnvelope;
+pub use effects::Preset;
